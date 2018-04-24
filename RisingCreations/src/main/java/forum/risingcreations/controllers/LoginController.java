30090d13b3cfcc626987259c0fffa6a4f675b43f
runi95 <rse005@uib.no>
2018-04-24 18:04:39 +0200

    Created very simple login pages

---
+++ RisingCreations/src/main/java/forum/risingcreations/controllers/LoginController.java
@@ -0,0 +1,19 @@
+package forum.risingcreations.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String postLoginData() {
+        return null;
+    }
+}
