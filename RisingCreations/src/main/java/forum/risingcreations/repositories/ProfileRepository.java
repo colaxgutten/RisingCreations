c2f912298369595405f68c79d7ce7bf026ae0530
runi95 <rse005@uib.no>
2018-04-24 19:42:08 +0200

    Created Profile table and repository

---
+++ RisingCreations/src/main/java/forum/risingcreations/repositories/ProfileRepository.java
@@ -0,0 +1,12 @@
+package forum.risingcreations.repositories;
+
+import forum.risingcreations.models.Profile;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ProfileRepository extends CrudRepository<Profile, Long> {
+
+    public List<Profile> findProfileByName(String name);
+
+}
---
+++ RisingCreations/src/main/java/forum/risingcreations/services/ProfileService.java
@@ -0,0 +1,23 @@
+package forum.risingcreations.services;
+
+import forum.risingcreations.models.Profile;
+import forum.risingcreations.repositories.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProfileService {
+
+    @Autowired
+    ProfileRepository profileRepository;
+
+    public Profile findProfileById(Long id) {
+        return profileRepository.findById(id).orElse(null);
+    }
+
+    public List<Profile> findProfileByName(String name) {
+        return profileRepository.findProfileByName(name);
+    }
+}
