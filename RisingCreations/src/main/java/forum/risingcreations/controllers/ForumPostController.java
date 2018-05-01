9ef9eb58301fd999ee518170bf7b634175d6b00d
runi95 <rse005@uib.no>
2018-05-01 21:13:24 +0200

    Created a page to post forum posts

--- RisingCreations/src/main/java/forum/risingcreations/controllers/ForumPostController.java
+++ RisingCreations/src/main/java/forum/risingcreations/controllers/ForumPostController.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Controller
 public class ForumPostController {
@@ -38,5 +39,47 @@ public class ForumPostController {
     byte[] getForumPostImage(@PathVariable("postid") Long postid) {
         Post p = postService.getById(postid);
         return p != null ? p.getImage() : new byte[]{};
+    }
+
+    @GetMapping("/post")
+    public String getCreateForumPost() {
+        return "createpost";
+    }
+
+    @PostMapping("/post")
+    public @ResponseBody String postCreateForumPost(@RequestParam("title") String title,
+                                                    @RequestParam("desc") String description,
+                                                    @RequestParam("img") MultipartFile imageFile) {
+        if(!title.isEmpty()) {
+            if(!description.isEmpty()) {
+                if(!imageFile.isEmpty()) {
+                    byte[] bytes = null;
+
+                    try {
+                        bytes = imageFile.getBytes();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    if(bytes != null) {
+                        Post post = new Post();
+                        post.setImage(bytes);
+                        post.setTitle(title);
+                        post.setDescription(description);
+
+                        postService.save(post);
+                    }
+
+                    return "Success!";
+                } else {
+                    return "You failed to upload " + title
+                            + " because the file was empty.";
+                }
+            } else {
+                return "Failed to upload because description was empty.";
+            }
+        } else {
+            return "Failed to upload because title was empty.";
+        }
     }
 }
