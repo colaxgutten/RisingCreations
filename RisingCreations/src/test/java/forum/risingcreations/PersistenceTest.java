f2ba14e7ce3766a42b82dda4be9107ae242734cf
runi95 <rse005@uib.no>
2018-05-01 16:57:41 +0200

    Fixed some broken tests

--- RisingCreations/src/test/java/forum/risingcreations/PersistenceTest.java
+++ RisingCreations/src/test/java/forum/risingcreations/PersistenceTest.java
@@ -2,29 +2,46 @@ package forum.risingcreations;
 
 import forum.risingcreations.models.Comment;
 import forum.risingcreations.models.Post;
-import forum.risingcreations.services.CommentService;
+import forum.risingcreations.repositories.PostRepository;
 import forum.risingcreations.services.PostService;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
-@DataJpaTest
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest
+@TestPropertySource("/testapplication.properties")
+@ExtendWith(SpringExtension.class)
 class PersistenceTest {
 
     @Autowired
     PostService postService;
 
     @Autowired
-    CommentService commentService;
+    PostRepository postRepository;
 
     @Test
-    private void testThePersistence() {
+    public void savingPostWillResultInOneEntity() {
         Post post = new Post();
-        post.setTitle("Hei");
+        post.setTitle("1");
+
+        postService.save(post);
+
+        Iterable<Post> iterable = postService.getAllPosts();
+        assertTrue(iterable.iterator().hasNext());
+
+        postService.delete(post);
+    }
+
+    @Test
+    public void savingPostPersistsComment() {
+        Post post = new Post();
+        post.setTitle("2");
 
         Comment comment = new Comment();
         comment.setDescription("This is a description");
@@ -33,7 +50,10 @@ class PersistenceTest {
 
         postService.save(post);
 
-        Post p = postService.getPostByTitle("Hei");
-        System.out.println("p(" + p + "), p.getComments(" + p.getComments() + "), p.getComments().size(" + p.getComments().size() + ")");
+        Post p = postService.getById(post.getId());
+        assertTrue(p != null);
+        assertEquals(1, p.getComments().size());
+
+        postService.delete(p);
     }
 }
