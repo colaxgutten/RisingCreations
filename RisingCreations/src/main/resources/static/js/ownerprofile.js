83e4c0faceb73e8460581467d4abf5cbf716f1e0
runi95 <rse005@uib.no>
2018-09-26 16:29:02 +0200

    Fixed problems with the new sidebar

---
+++ RisingCreations/src/main/resources/static/js/ownerprofile.js
@@ -0,0 +1,8 @@
+function editName() {
+    document.getElementById("get_name").style.display="block";
+}
+function editImg() {
+    document.getElementById("get_file").style.display="block";
+}
+
+$('#profileli').addClass("active");
