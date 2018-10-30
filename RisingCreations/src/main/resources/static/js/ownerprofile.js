baec337173adb2158460022dfe262ebc6472599c
runi95 <rse005@uib.no>
2018-10-30 14:50:15 +0100

    You can now edit name and profile image by clicking on them

--- RisingCreations/src/main/resources/static/js/ownerprofile.js
+++ RisingCreations/src/main/resources/static/js/ownerprofile.js
@@ -1,8 +1,25 @@
 function editName() {
-    document.getElementById("get_name").style.display="block";
+    document.getElementById("namecontainer").style.display = "none";
+    var nameInput = document.getElementById("get_name");
+    nameInput.value = document.getElementById("name").innerText;
+    nameInput.style.display = "block";
+    nameInput.focus();
 }
+
+function cancelName() {
+    document.getElementById("namecontainer").style.display = "block";
+    document.getElementById("get_name").style.display = "none";
+}
+
 function editImg() {
-    document.getElementById("get_file").style.display="block";
+    document.getElementById("get_file").click();
+    // document.getElementById("get_file").style.display="block";
 }
+
+$(document).keyup(function (e) {
+    if (e.keyCode == 27) { // escape key maps to keycode `27`
+        cancelName();
+    }
+});
 
 $('#profileli').addClass("active");
